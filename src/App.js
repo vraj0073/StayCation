43fdf03c9a10524324d1b0d892d2e1456c0fd040
Het Ketanbhai Shah <ht699147@dal.ca>
2022-03-28 15:48:02 -0300

    Corrections in app.js import

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
-import { Container } from 'react-bootstrap';
-import './App.css';
-import BlogPage from './Assets/pages/BlogPage'; 
-import SearchPage from './Assets/pages/SearchPage';
-import Home from './Assets/pages/Home';
+import { Container } from "react-bootstrap";
+import "./App.css";
+import BlogPage from "./Assets/pages/blogPage";
+import SearchPage from "./Assets/pages/SearchPage";
+import Home from "./Assets/pages/Home";
 
 import { Routes, Route, Link, BrowserRouter } from "react-router-dom";
 import TravelHistory from "./Assets/pages/TravelHistory";
@@ -10,23 +10,20 @@ import WishList from "./Assets/pages/WishList";
 
 function App() {
   return (
-    <div className='App'>
-    <BrowserRouter>
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/blogs" element={<BlogPage />} />
-      <Route path="/search" element={<SearchPage />} />
-       <Route
+    <div className="App">
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/blogs" element={<BlogPage />} />
+          <Route path="/search" element={<SearchPage />} />
+          <Route
             path="th"
             element={<TravelHistory email="vrajjadhav0073@gmail.com" />}
           />
           <Route path="wl" element={<WishList />} />
-
-    </Routes>
-  </BrowserRouter>
-</div>
+        </Routes>
+      </BrowserRouter>
+    </div>
   );
-  
-  }
+}
 export default App;
-
