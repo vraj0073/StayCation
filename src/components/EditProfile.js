9b30c0455724126c67213fe5077cac8f98b98ac9
vraj jadhav <vr8804417@dal.ca>
2022-03-29 01:52:23 -0300

    changes to url

--- src/components/EditProfile.js
+++ src/components/EditProfile.js
@@ -1,3 +1,7 @@
+/*
+Author: Vraj Jadhav
+Description: This component handle edit profile page.
+*/
 import React, { useEffect, useState } from 'react'
 import '../css/Header.css'
 import 'react-bootstrap'
@@ -22,8 +26,8 @@ export const EditProfile = () => {
   const history = useNavigate();
   const {state} = useLocation();
   const sendData = () =>{
-    console.log("image here"+image)
-    axios.post('http://localhost:5000/profileedit', {
+    
+    axios.post('https://weba3b00886409.herokuapp.com/profileedit', {
           bio: bio,
           email: email,
           phone: phone,
@@ -34,41 +38,41 @@ export const EditProfile = () => {
         })
         .then(function (response) {
           console.log(response);
-          
+          history('/Profile',{state:email})
         })
         .catch(function (error) {
           console.log(error);
         });
   }
   const imageUpload = (e) =>{
-    console.log(e.target.files[0]);
+    
     setimage(e.target.files[0]);
   }
   const validbio= (e) => {
     const bio1 = e.target.value;
     setbio(bio1)
-    console.log(bio)
+    
      
      
   };
   const validemail= (e) => {
     const email1 = e.target.value;
     setemail(email1)
-    console.log(email)
+   
      
      
   };
   const validphone= (e) => {
     const phone1 = e.target.value;
     setphone(phone1)
-    console.log(phone)
+    
      
      
   };
   const validlivesin= (e) => {
     const lives1 = e.target.value;
     setlivesin(lives1)
-    console.log(lives1)
+    
      
      
   };
@@ -76,26 +80,19 @@ export const EditProfile = () => {
     
     const speaks1 = e.target.value;
     setspeaks(speaks1)
-    console.log(speaks)
-     
+    
   };
   const validwork= (e) => {
     const work1 = e.target.value;
     setworks(work1)
-    console.log(works)
+ 
      
      
   };
   const onSubmit = (e) =>{
-    console.log(email)
-    console.log(bio)
-    console.log(livesin)
-    console.log(speaks)
-    console.log(works)
-    console.log(phone)
-    console.log("image here"+image)
+    
+    
     sendData();
-    history('/Profile',{state:email})
   }
   return (
       <>
