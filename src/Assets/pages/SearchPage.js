a57ae8838746f9130e7a188d3fc37f885de33e65
Kishan <ks805556@dal.ca>
2022-03-31 11:33:25 -0300

    update searchPage

--- src/Assets/pages/SearchPage.js
+++ src/Assets/pages/SearchPage.js
@@ -9,20 +9,26 @@ import Image from 'react-bootstrap/Image'
 import '../../css/Home.css'
 import '../../css/Header.css'
 import 'react-bootstrap'
+import '../../css/SearchPage.css'
 
 export const SearchPage = () => {
 
+  const [location, setLocation]=useState('');
+  const [duration,setDuration]=useState('Days')
+  const [checkInDate, setCheckInDate] = useState(new Date());
+  const [checkOutDate, setCheckOutDate] = useState(new Date());
+  async function simpleSearch(e,value){
+    e.preventDefault();
+    console.log("simple search :"+value );
+  }
+
   return (
-    <>
-    <div >
-    
+   <>
     <div className='col'  >
       
       <div className="header" >
         <div className="header-items">
-            
           <button>S</button>taycation
-          
           <Navbar collapseOnSelect expand="lg" >
               <Container className='d-flex' id='homenavigation'>
                   <Navbar.Brand href="#home" className='header-navbar'>Home</Navbar.Brand>
@@ -42,18 +48,57 @@ export const SearchPage = () => {
               </Container>
           </Navbar>
         </div>    
-      </div>
-    
+      </div>  
     </div>
-    
-    <h2 className='title'>Search Under Construction</h2>
-    <div className="section">
+    <div>
+
+    <h2 className='title'>Simple Search</h2>
+    <div className="input-group mb-3" style={{paddingLeft:"25px",paddingTop:"25px"}}>
+  <input 
+    type="text" 
+    className="simplesearch" 
+    placeholder="e.g Halifax..." 
+    aria-label="simpleSearch" 
+    aria-describedby="basic-addon2"
+    onChange={(e)=>{setSearchValue(e.target.value)}}
+  />
+  <div className="input-group-append">
+    <button className="btn btn-outline-secondary" type="button"
+      onClick={(e)=>{simpleSearch(e,searchValue)}}
+      onSubmit={simpleSearch}>Search
+    </button>
+  </div>
+    </div>
+    <br/>
+    <div>
+    <h2 className='title'>Custom Search</h2>
+      <Form>
+        <Form.Group className="mb-3" controlId="formBasicEmail">
+          <Form.Label className='labelTxt'>
+            Location
+          </Form.Label>
+          <div style={{paddingLeft:"20px"}}>
+          <Form.Control style={{width:"500px",height:"40px"}} onChange={(e)=>{setLocation(e.target.value)}} type="email" placeholder="e.g Halifax" />
           </div>
-        
+         </Form.Group>
+
+        <Form.Group className="mb-3" controlId="formBasicPassword">
+          <Form.Label className='labelTxt'>Password</Form.Label>
+          <Form.Control type="password" placeholder="Password" />
+        </Form.Group>
+        <Form.Group className="mb-3" controlId="formBasicCheckbox">
+          <Form.Check type="checkbox" label="Check me out" />
+        </Form.Group>
+        <Button variant="primary" type="submit">
+          Submit
+        </Button>
+      </Form>
     </div>
+
+</div>
      
-    </>
-    )}
+  </>
+  )}
 
 export default SearchPage;
 
