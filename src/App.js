b9ca3d0467db544ad1653d01e399a74bf91fbc8c
Kishan <ks805556@dal.ca>
2022-04-05 18:07:02 -0300

    update from master

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import TravelHistory from "./Assets/pages/TravelHistory";
 import WishList from "./Assets/pages/WishList";
 import React from "react";
 import RegistrationPage from "./components/RegistrationPage";
-import 'bootstrap/dist/css/bootstrap.min.css';
+import "bootstrap/dist/css/bootstrap.min.css";
 import { Login } from "./components/Login";
 import Forget_Password_Email from "./components/Forget_Password_Email";
 import New_Password from "./components/New_Password";
@@ -17,34 +17,35 @@ import Home from "./components/Home";
 import Review from "./Assets/pages/Review";
 import ReviewEdit from "./Assets/pages/ReviewEdit";
 import ReviewWrite from "./Assets/pages/ReviewWrite";
-import SearchResult from "./Assets/pages/SearchResult";
+
 function App() {
   return (
     <div>
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Home />} />
-          <Route exact path='/home' element={<Home/>}></Route>
-          <Route exact path='/blogs/home' element={<Home/>}></Route>
-          <Route exact path='/Register' element={<RegistrationPage/>}></Route>
-          <Route exact path='/Login' element={<Login/>}></Route>
-          <Route path='/Profile' element={<Customer/>} ></Route>
-          <Route path='/Editprofile' element={<EditProfile/>} ></Route>
-          <Route path='/Resetpassword' element={<New_Password/>} ></Route>
-          <Route path='/Forgetpassword' element={<Forget_Password_Email/>} ></Route>
-          <Route path='/Logout' element={<Login/>} ></Route>
+          <Route exact path="/home" element={<Home />}></Route>
+          <Route exact path="/blogs/home" element={<Home />}></Route>
+          <Route exact path="/Register" element={<RegistrationPage />}></Route>
+          <Route exact path="/Login" element={<Login />}></Route>
+          <Route path="/Profile" element={<Customer />}></Route>
+          <Route path="/Editprofile" element={<EditProfile />}></Route>
+          <Route path="/Resetpassword" element={<New_Password />}></Route>
+          <Route
+            path="/Forgetpassword"
+            element={<Forget_Password_Email />}
+          ></Route>
+          <Route path="/Logout" element={<Login />}></Route>
           <Route path="/blogs" element={<BlogPage />} />
           <Route path="/search" element={<SearchPage />} />
-          <Route path="/searchresults" element={<SearchResult />} />
-
-         <Route
+          <Route
             path="th"
             element={<TravelHistory email="vrajjadhav0073@gmail.com" />}
           />
-          <Route path="wl" element={<WishList />} />
-            <Route path="/user-reviews" element={<Review/>}></Route>
-            <Route path="/edit-review" element={<ReviewEdit/>}></Route>
-            <Route path="/write-review" element={<ReviewWrite/>}></Route>
+          <Route path="wishlist" element={<WishList />} />
+          <Route path="/user-reviews" element={<Review />}></Route>
+          <Route path="/edit-review" element={<ReviewEdit />}></Route>
+          <Route path="/write-review" element={<ReviewWrite />}></Route>
         </Routes>
       </BrowserRouter>
     </div>
