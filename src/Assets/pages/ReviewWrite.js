c1587326b3f97313715ac41e0395b02f34358a14
Qinyue Wang <qn642785@dal.ca>
2022-04-07 10:25:39 -0400

    Modify review feature and add help center feature

--- src/Assets/pages/ReviewWrite.js
+++ src/Assets/pages/ReviewWrite.js
@@ -5,13 +5,20 @@ import {AiFillStar, AiOutlineStar} from "react-icons/ai"
 import {useNavigate} from "react-router-dom";
 import '../../css/ReviewEdit.css'
 import "../../css/Header.css"
-
+import Navheader from "../../components/Navheader";
 
 function ReviewWrite() {
-    let userEmail = "qqq@gmail.com";
+    let userEmail = localStorage.getItem("userEmail");
+    // let userEmail = "qqq@gmail.com";
     let roomId = "145";
     let roomName = "Charming Astoria studio on quiet tree-lined street";
-    let date = "2021-06-08";
+
+    let currentDate =new Date();
+    let day = currentDate.getDate();
+    let month = currentDate.getMonth() + 1;
+    let year = currentDate.getFullYear();
+    let date = year+"-"+month+"-"+day;
+
     const [number, setNumber] = useState(0);
     const [star, setStar] = useState(undefined);
     const [newContent, setNewContent] = useState('');
@@ -33,71 +40,68 @@ function ReviewWrite() {
     }
 
     return (
-        <div className="review-edit" onSubmit={submit}>
-            <div className="header">
-                <div className="header-items">
-                    <button>S</button>
-                    taycation
+        <>
+            <Navheader/>
+            <div className="review-edit" onSubmit={submit}>
+                <div className="re-house-name">
+                    <h1 className="re-house-name-h1">A Review For {roomName}</h1>
                 </div>
-                <div className="header-items">Qinyue</div>
-            </div>
-            <div className="re-house-name">
-                <h1 className="re-house-name-h1">A Review For {roomName}</h1>
-            </div>
-            <div className="re-star-evaluation">
-                {Array(5).fill().map((_, index) => number >= index + 1 || star >= index + 1 ? (<AiFillStar
-                    name="starEvaluation"
-                    onMouseOver={() => !number && setStar((index + 1))}
-                    onMouseLeave={() => setStar(undefined)}
-                    style={{color: "orange"}}
-                    onClick={() => {
-                        setNumber(index + 1);
-                    }}
-                />) : (<AiOutlineStar
-                    onMouseOver={() => !number && setStar((index + 1))}
-                    onMouseLeave={() => setStar(undefined)}
-                    style={{color: "orange"}}
-                    onClick={() => {
-                        setNumber(index + 1);
-                    }}
-                />))}
-            </div>
-            <div className="re-edit-content">
-                <div className="re-text">
-                    <textarea placeholder="Please write a review" className="re-textarea" name="content-text" onChange={changeContent}/>
-                    {isSubmitted && error.length > 0 && <p className="re-text-p">{error}</p>}
+                <div className="re-star-evaluation">
+                    {Array(5).fill().map((_, index) => number >= index + 1 || star >= index + 1 ? (<AiFillStar
+                        name="starEvaluation"
+                        onMouseOver={() => !number && setStar((index + 1))}
+                        onMouseLeave={() => setStar(undefined)}
+                        style={{color: "orange"}}
+                        onClick={() => {
+                            setNumber(index + 1);
+                        }}
+                    />) : (<AiOutlineStar
+                        onMouseOver={() => !number && setStar((index + 1))}
+                        onMouseLeave={() => setStar(undefined)}
+                        style={{color: "orange"}}
+                        onClick={() => {
+                            setNumber(index + 1);
+                        }}
+                    />))}
                 </div>
-                <div className="re-edit-buttons">
-                    <button className="re-edit-btn" type="submit" onClick={() => {
-                        setIsSubmitted(true);
-                        if (newContent !== '' && number !== 0) {
-                            console.log(number);
-                            axios.post("https://node-a3.herokuapp.com/review/post", {
-                                userEmail: userEmail,
-                                roomId: roomId,
-                                roomName: roomName,
-                                content: newContent,
-                                rating: number,
-                                date: date
-                            })
-                                .then((response) => {
-                                    console.log(response.data);
-                                });
-                            navigate("/user-reviews");
-                        } else if (newContent === '') {
-                            setError("The review should be 10 letters at least");
-                        } else if (number === 0) {
-                            setError("Star rating cannot be 0")
-                        }
-                    }}>Save
-                    </button>
-                    <button className="re-edit-btn" type="submit" onClick={() => {
-                        navigate("/user-reviews")
-                    }}>Cancel
-                    </button>
+                <div className="re-edit-content">
+                    <div className="re-text">
+                        <textarea placeholder="Please write a review" className="re-textarea" name="content-text"
+                                  onChange={changeContent}/>
+                        {isSubmitted && error.length > 0 && <p className="re-text-p">{error}</p>}
+                    </div>
+                    <div className="re-edit-buttons">
+                        <button className="re-edit-btn" type="submit" onClick={() => {
+                            setIsSubmitted(true);
+                            if (newContent !== '' && number !== 0) {
+                                console.log(number);
+                                axios.post("https://node-a3.herokuapp.com/review/post", {
+                                    userEmail: userEmail,
+                                    roomId: roomId,
+                                    roomName: roomName,
+                                    content: newContent,
+                                    rating: number,
+                                    date: date
+                                })
+                                    .then((response) => {
+                                        console.log(response.data);
+                                    });
+                                navigate("/user-reviews");
+                            } else if (newContent === '') {
+                                setError("The review should be 10 letters at least");
+                            } else if (number === 0) {
+                                setError("Star rating cannot be 0")
+                            }
+                        }}>Save
+                        </button>
+                        <button className="re-edit-btn" type="submit" onClick={() => {
+                            navigate("/user-reviews")
+                        }}>Cancel
+                        </button>
+                    </div>
                 </div>
             </div>
-        </div>)
+        </>)
 }
 
 export default ReviewWrite;
